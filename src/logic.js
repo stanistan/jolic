feface301611b4aace7d0a198014de8fd4ec26d7
Stan Rozenraukh <stan@stanistan.com>
2012-12-17 12:03:49 -0500

    done some work, but things dont work

--- src/logic.js
+++ src/logic.js
@@ -99,6 +99,185 @@ function reifyName(n) {
   return new LogicVar("_." + n);
 }
 
+function delay(f) {
+  return function() {
+    return f;
+  };
+}
+
+function eq(val1, val2) {
+  return function(s) {
+    console.log('val1: ', val1)
+    console.log('val2: ', val2)
+    return unify(val1, val2, s);
+  }
+}
+
+function either() {
+  var goals = [].slice.call(arguments, 0);
+
+  return function(s) {
+    var r = goals.length
+      ? delay(function() {
+          return [
+              _.first(goals)(s)
+            , delay(function() { return either.apply(null, _.rest(goals)); })
+          ];
+        })
+      : delay(mzero);
+
+    console.log('end of either', r());
+    return r;
+    // if (!goals.length) return empty;
+    //  var r = [_.first(goals)(s), delay(function() { return either.apply(null, _.rest(goals))})]
+    //  console.log('heererere', r);
+    //  return r;
+    // // var r =  goals.map(function(a) {
+    // //   // console.log('goal:', a)
+    // //   return a(s)
+    // // })
+    // // // console.log('either goals: ', r)
+    // // r = [].concat.apply([], r);
+    // // return [].concat.apply([], r)
+    // // re
+  }
+}
+
+function mzero() {
+  return false;
+}
+
+function choice(a, b) {
+  return [a, b];
+}
+
+function ccase(thing, actions) {
+
+  var action, args = [];
+  if (!!thing.length || !thing) {
+    action = '_';
+  } else if (_.isFunction(thing)) {
+    action = 'f_';
+    args = [thing];
+  } else if (nonEmptyArr(thing) && _.isFunction(thing[1])) {
+    action = 'a_f';
+    args = thing;
+  } else {
+    action = 'a';
+    args = [thing];
+  }
+
+  return actions[action].apply(null, args);
+}
+
+function bind(thing, goal) {
+  return ccase(thing, {
+      _: mzero
+    , f_: function(f_) {
+        return function() {
+          bind((f_), goal)
+        }
+      }
+    , a: function(a) {
+        return goal(a)
+      }
+    , a_f: function(a, f) {
+        return mplus(goal(a), function() {
+          return bind((f), g);
+        });
+      }
+  });
+}
+
+function bindLazy(e) {
+  var args = slice(arguments, 1);
+  if (!args.length) return e;
+  args[0] = bind(e, args[0]);
+  return bindLazy.apply(null, args);
+}
+
+function mplus(thing, f) {
+  return ccase(thing, {
+      _: function() {
+        return f();
+      }
+    , f_: function(f_) {
+        return function() {
+          return mplus(f(), f_);
+        };
+      }
+    , a: function(a) {
+        return choice(a, f);
+      }
+    , a_f: function(a, f_) {
+        return choice(a, function() {
+          return mplus(f(), f_);
+        });
+      }
+  });
+}
+
+function mplusLazy(thing) {
+  var rest = slice(arguments, 1);
+  return rest.length
+    ? mplus(thing, function() { return mplusLazy.apply(null, rest); })
+    : thing;
+}
+
+function fresh(vars) {
+  var fs = slice(arguments, 1);
+
+  return function(a) {
+    delay(function() {
+
+    })
+  }
+}
+
+function run(v /*, ... goals */) {
+
+  v = islvar(v) ? v : lvar(v);
+
+  var goals = slice(arguments, 1);
+
+
+
+  var s = empty
+    , re = []
+  for (var i = 0; i < goals.length; i++) {
+    s = goals[i](s);
+    re.push(reify(v, s))
+  }
+  console.log('finishing:', s)
+  return re
+}
+
+
+// console.log(eq(lvar('q'), 1)(empty))
+
+// var q = lvar('q');
+
+// console.log(run(q, eq(q, 1)));
+
+// console.log(
+//   run(q
+//     ,either(
+//       eq(q, 1),
+//       eq(q, 2)
+//     ))
+// )
+
+// console.log(mplus(false, function() {
+//   return 'a';
+// }));
+
+function slice(a, n) {
+  n = n || 0;
+  return [].slice.call(a, n);
+}
+
+
+
 exports.empty = empty;
 exports.lvar = lvar;
 exports.islvar = islvar;
@@ -111,3 +290,5 @@ exports.extS = extS;
 exports.unify = unify;
 exports.walk_ = walk_;
 exports.reifyS = reifyS;
+// exports.eq = eq;
+// exports.delay
